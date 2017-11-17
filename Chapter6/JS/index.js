bf7ed64e2c0e9e9781536f72333ec441739fe023
Julian Hernández <juliusvernedesign@gmail.com>
2017-11-17 17:42:17 -0500

    Drawing with SVG

--- Chapter6/JS/index.js
+++ Chapter6/JS/index.js
@@ -1,7 +1,7 @@
 var dataset = []
 
-for (var i = 0; i < 25; i++) {
-  dataset.push(Math.floor((Math.random() * 200) + 50));
+for (var i = 0; i < 20; i++) {
+  dataset.push(Math.floor((Math.random() * 100) + 5));
 }
 
 //Loading CSV
@@ -59,6 +59,8 @@ console.log(data);
 
 //d3.select("body").append("p").text("New Paragraph!");
 
+//Create a very basic Bar chart using D3
+/*
 d3.select("body").selectAll("div")
       .data(dataset)
       .enter()
@@ -69,4 +71,50 @@ d3.select("body").selectAll("div")
       })
       .style('background-color', function(d){
         return "rgba(255,"+ d/4 +","+ (d/4)*5 +", 1.0)";
+      });
+*/
+
+//Create Circles using SVG and assign attributes using D3
+/*
+var w = 500;
+var h = 50;
+
+var svg =
+d3.select('body')
+  .append('svg')
+  .attr('width', w)
+  .attr('height', h);
+
+var circles =
+svg.selectAll('circle')
+  .data(dataset)
+  .enter()
+  .append('circle');
+
+circles.attr('cx', function(d,i) {
+  return (i*50) +25;})
+  .attr('cy', h/2)
+  .attr('r', function(d) {
+  return d;})
+  .attr('fill', 'yellow')
+  .attr('stroke', 'orange')
+  .attr('stroke-width', function (d) {
+  return d/5;
+}); */
+
+//A More advanced bar chart using D3
+
+
+
+d3.select("body").selectAll("div")
+      .data(dataset)
+      .enter()
+      .append("div")
+      .attr("class", "bar")
+      .style('height', function(d) {
+        var barHeight = d * 5;
+        return barHeight + "px";
+      })
+      .style('background-color', function(d){
+        return "rgba("+255/(d/6)+","+ d +","+ (d/8)*5 +", 1.0)";
       });
