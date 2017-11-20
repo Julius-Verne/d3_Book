388ec9c523a66eb5abfcd0040874893a947c918e
Julian Hernández <juliusvernedesign@gmail.com>
2017-11-20 14:43:19 -0500

    Chapter 6 Finished
    - Created Basic Bar Graphs using D3.
    - Created Basic Scatterplots
    - Used Labels to include extra info
    - Worked with SVG to convey info

--- Chapter6/JS/index.js
+++ Chapter6/JS/index.js
@@ -1,8 +1,10 @@
 var dataset = []
 
 for (var i = 0; i < 20; i++) {
-  dataset.push(Math.floor((Math.random() * 100) + 5));
-}
+  var x = Math.floor((Math.random() * 480) + 20);
+  var y = Math.floor((Math.random() * 200) + 20);
+  dataset.push([x,y]);
+};
 
 //Loading CSV
 /*
@@ -102,10 +104,8 @@ circles.attr('cx', function(d,i) {
   return d/5;
 }); */
 
-//A More advanced bar chart using D3
-
-
-
+//A More advanced bar chart using D3 but that can be improved upon
+/*
 d3.select("body").selectAll("div")
       .data(dataset)
       .enter()
@@ -118,3 +118,98 @@ d3.select("body").selectAll("div")
       .style('background-color', function(d){
         return "rgba("+255/(d/6)+","+ d +","+ (d/8)*5 +", 1.0)";
       });
+*/
+
+//The New bar chart with improvements. It uses SVG and appends info to the chart to better convey info.
+/*
+var w = 500;
+var h = 100;
+var barPadding = 1;
+
+var svg = d3.select('body')
+          .append('svg')
+          .attr('width',w)
+          .attr('height', h);
+
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr('x', function(d, i) {
+      return (w/dataset.length)*i;
+    })
+    .attr('y', function (d) {
+      return h-d;
+    })
+    .attr('width', w/dataset.length-barPadding)
+    .attr('height', function (d) {
+      return d;
+    })
+    .attr('fill', function(d){
+      return "rgba("+255/(d/6)+","+ d +","+ (d/8)*5 +", 1.0)";
+    });
+
+svg.selectAll('text')
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function (d) {
+      return d;
+    })
+    .attr('x', function(d, i) {
+      return i*(w/dataset.length)+(w/dataset.length-barPadding)/2;
+    })
+    .attr('y', function (d) {
+      return (h-d)+14;
+    })
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', '11px')
+    .attr('fill', 'white')
+    .attr('text-anchor', 'middle');
+*/
+
+//Scatterplot using D3
+
+var w = 500;
+var h = 200;
+var barPadding = 1;
+
+var svg = d3.select('body')
+          .append('svg')
+          .attr('width', w)
+          .attr('height', h);
+
+svg.selectAll('circle')
+  .data(dataset)
+  .enter()
+  .append('circle')
+  .attr('cx', function(d, i) {
+    console.log(d);
+    return (d[0]);
+  })
+  .attr('cy', function (d) {
+    return d[1];
+  })
+  .attr('r',function (d) {
+    return Math.sqrt(h-d[1]);
+  })
+  .attr('fill', function(d){
+    return "rgba("+255/(d[0]/6)+","+ d[1] +","+ (d[1]/8)*5 +", 1.0)";
+  });
+
+  svg.selectAll('text')
+    .data(dataset)
+    .enter()
+    .append('text')
+    .text(function (d) {
+      return d[0]+","+d[1];
+    })
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', '10px')
+    .attr('fill', 'teal')
+    .attr('x', function (d) {
+      return d[0]+15;
+    })
+    .attr('y', function (d) {
+      return d[1]+5;
+    })
