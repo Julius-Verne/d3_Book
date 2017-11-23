78077cf1b047e66e02593665dede6c7830d3cebe
Julian Hernández <juliusvernedesign@gmail.com>
2017-11-23 10:12:40 -0500

    Revert "Chapter 7"
    This reverts commit f445154bef0cc0e8883f601da6aabacd0ba4f97b.

--- Chapter7_timeScale/JS/index.js
+++ Chapter7_timeScale/JS/index.js
@@ -13,80 +13,31 @@ var margin = {top: 20, right: 30, bottom: 20, left: 30};
 var w = 600 - margin.left - margin.right;
 var h = 400 - margin.top - margin.bottom;
 
-var dataset, xScale, yScale;  //Empty, for now
+d3.csv("../time_scale_data.csv", function(d) {
 
-//For converting strings to Dates
-var parseTime = d3.timeParse("%m/%d/%y");
+  rowConverter(d);
+  console()
+  dataset=d;
+  for (var i = 0; i < dataset.length; i++) {
+    rowConverter(dataset[i]);
+}
+});
 
-//For converting Dates to strings
-var formatTime = d3.timeFormat("%b %e");
-
-//Function for converting CSV values from strings to Dates and numbers
-var rowConverter = function(d) {
+var rowConverter = function (d) {
+  console.log(d);
   return {
     Date: parseTime(d.Date),
     Amount: parseInt(d.Amount)
   };
-}
+};
 
-//Load in the data
-d3.csv("time_scale_data.csv", rowConverter, function(data) {
+var parseTime = d3.timeParse("%m/%d/%y");
 
-  //Copy data into global dataset
-  dataset = data;
-
-  //Create scale functions
-  xScale = d3.scaleTime()
-           .domain([
-            d3.min(dataset, function(d) { return d.Date; }),
-            d3.max(dataset, function(d) { return d.Date; })
-          ])
-           .range([padding, w - padding]);
-
-  yScale = d3.scaleLinear()
-           .domain([
-            d3.min(dataset, function(d) { return d.Amount; }),
-            d3.max(dataset, function(d) { return d.Amount; })
-          ])
-           .range([h - padding, padding]);
-
-  //Create SVG element
-  var svg = d3.select("body")
-        .append("svg")
-        .attr('width', w + margin.left + margin.right)
-        .attr('height', h + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")"
-
-  //Generate date labels first, so they are in back
-  svg.selectAll("text")
-     .data(dataset)
-     .enter()
-     .append("text")
-     .text(function(d) {
-        return formatTime(d.Date);
-     })
-     .attr("x", function(d) {
-        return xScale(d.Date) + 4;
-     })
-     .attr("y", function(d) {
-        return yScale(d.Amount) + 4;
-     })
-     .attr("font-family", "sans-serif")
-     .attr("font-size", "11px")
-     .attr("fill", "#bbb");
-
-  //Generate circles last, so they appear in front
-  svg.selectAll("circle")
-     .data(dataset)
-     .enter()
-     .append("circle")
-     .attr("cx", function(d) {
-        return xScale(d.Date);
-     })
-     .attr("cy", function(d) {
-        return yScale(d.Amount);
-     })
-     .attr("r", 2);
-
-});
+var xScale=
+d3.scaleTime()
+    .domain([d3.min(dataset, function (d) {
+      return d.Date;
+    }), d3.max(dataset, function (d) {
+      return d.Amount;
+    }) ])
+    .range([0 , w]);
